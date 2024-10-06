7c0a93304c28c2a687ba50c53699a69880c6b33e
emon02 <abdulalimemon11@gmail.com>
2024-10-07 02:07:28 +0600

    cors update

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://shine-store.vercel.app/"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // MongoDB Connection URL
